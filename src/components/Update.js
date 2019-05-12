b705600c22b367f11e6b48b732885f272e5f2f8a
sraya avraham <sraya305@gmail.com>
2019-05-12 16:37:23 +0300

    fix update

--- src/components/Update.js
+++ src/components/Update.js
@@ -1,40 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Update.css";
 import Marquee from "react-text-marquee";
+import axios from "axios";
+import Ticker from "react-ticker";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
 
 function Update() {
-  const [updates, setUpdates] = useState([
-    {
-      text: "מתקיים החלק השני של סדנת הערכת המצב ",
-      date: new Date()
-    },
-    {
-      text: "מתקיים החלק השני של סדנת הערכת המצב ",
-      date: new Date()
-    },
-    {
-      text: "מתקיים החלק השני של סדנת הערכת המצב ",
-      date: new Date()
-    },
-    {
-      text: "מתקיים החלק השני של סדנת הערכת המצב ",
-      date: new Date()
-    },
-    {
-      text: "מתקיים החלק השני של סדנת הערכת המצב ",
-      date: new Date()
-    }
-  ]);
+  const inputValue = "";
+  const [updates, setUpdates] = useState([]);
+  useEffect(async () => {
+    const result = await axios("http://localhost:8000/api/updates/all");
+    setUpdates(result.data.updates);
+  }, []);
 
+  function handleClick() {
+    const { value: update } = Swal.fire({
+      title: "Enter your update text",
+      input: "text",
+      inputValue: inputValue,
+      showCancelButton: true,
+      inputValidator: value => {
+        if (!value) {
+          return "You need to write something!";
+        }
+      }
+    });
+
+    if (update) {
+      Swal.fire(`Your IP address is ${update}`);
+    }
+  }
   return (
-    <div className="update">
+    <div className="update" onClick={handleClick}>
       <div className="update-title">עידכונים</div>
-      <Marquee
+      {/* <Marquee
         className="update-marquee"
         text={updates.map(update => update.text).join(" | ")}
         loop={true}
         hoverToStop={true}
-      />
+      /> */}
+      <p className="marquee">
+        <span>{updates.map(update => update.text).join(" | ")}</span>
+      </p>
     </div>
   );
 }
